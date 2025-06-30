faca9a30d2b46a193782fe6f0385f3194c44dc15
jscamposx <jscamposx@gmail.com>
2025-06-30 15:32:14 -0700

    definiendo estructura del front

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import PaqueteDetalle from './pages/PaqueteDetalle';
+import PaqueteDetalle from './pages/[url]';
 
 function App() {
   return (
