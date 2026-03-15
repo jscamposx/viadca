48b7f50ad0e1b0c4aafde227bdbd314246b4c285
Jesus Campos <jscamposx@gmail.com>
2026-03-15 08:26:27 -0600

    feat: s

---
+++ src/features/home/pages/HomePaymentRequiredPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const HomePaymentRequiredPage = () => {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#ffffff",
+        color: "#111111",
+        margin: 0,
+        padding: "1rem",
+        textAlign: "center",
+      }}
+    >
+      <h1 style={{ fontSize: "1.5rem", fontWeight: 500 }}>Payment is required.</h1>
+    </main>
+  );
+};
+
+export default HomePaymentRequiredPage;
